494848ca84bd2f3d99b2947327f1016f14507c20
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-03-18 05:20:38 +0000

    ch 1

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require "sinatra"
 require "sinatra/reloader"
+require 'securerandom'
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+# Генерируем случайный пароль
+def generate_password(length, include_lower_case, include_upper_case, include_numbers, include_special_characters)
+  characters = []
+  characters += ('a'..'z').to_a if include_lower_case
+  characters += ('A'..'Z').to_a if include_upper_case
+  characters += (0..9).to_a if include_numbers
+  characters += %w(! @ # $ % ^ & *) if include_special_characters
+
+  password = ''
+  length.times { password += characters.sample.to_s }
+  password
+end
+
+get '/' do
+  erb :index
+end
+
+post '/generate' do
+  length = params[:length].to_i
+  include_lower_case = params[:lower_case] == 'on'
+  include_upper_case = params[:upper_case] == 'on'
+  include_numbers = params[:numbers] == 'on'
+  include_special_characters = params[:special] == 'on'
+
+  @password = generate_password(length, include_lower_case, include_upper_case, include_numbers, include_special_characters)
+  erb :result
 end
