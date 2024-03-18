80efaa54475be54817d269c37e00c45330c000e2
Mariia Nychyporenko <150645556+wrynych@users.noreply.github.com>
2024-03-18 05:37:50 +0000

    ch 2

--- app.rb
+++ app.rb
@@ -5,27 +5,35 @@ require 'securerandom'
 # Генерируем случайный пароль
 def generate_password(length, include_lower_case, include_upper_case, include_numbers, include_special_characters)
   characters = []
-  characters += ('a'..'z').to_a if include_lower_case
-  characters += ('A'..'Z').to_a if include_upper_case
-  characters += (0..9).to_a if include_numbers
-  characters += %w(! @ # $ % ^ & *) if include_special_characters
+  characters += ('a'..'z').to_a if include_lower_case || (!include_lower_case && !include_upper_case && !include_numbers && !include_special_characters)
+  characters += ('A'..'Z').to_a if include_upper_case || (!include_lower_case && !include_upper_case && !include_numbers && !include_special_characters)
+  characters += (0..9).to_a if include_numbers || (!include_lower_case && !include_upper_case && !include_numbers && !include_special_characters)
+  characters += %w(! @ # $ % ^ & *) if include_special_characters || (!include_lower_case && !include_upper_case && !include_numbers && !include_special_characters)
 
   password = ''
   length.times { password += characters.sample.to_s }
   password
 end
 
+
 get '/' do
   erb :index
 end
 
 post '/generate' do
-  length = params[:length].to_i
-  include_lower_case = params[:lower_case] == 'on'
-  include_upper_case = params[:upper_case] == 'on'
-  include_numbers = params[:numbers] == 'on'
-  include_special_characters = params[:special] == 'on'
+  @length = params[:length].to_i
+  @include_lower_case = params[:lower_case] == 'on'
+  @include_upper_case = params[:upper_case] == 'on'
+  @include_numbers = params[:numbers] == 'on'
+  @include_special_characters = params[:special] == 'on'
 
-  @password = generate_password(length, include_lower_case, include_upper_case, include_numbers, include_special_characters)
+  if params[:basic]
+    @include_lower_case = true
+    @include_upper_case = true
+    @include_numbers = true
+    @include_special_characters = true
+  end
+
+  @password = generate_password(@length, @include_lower_case, @include_upper_case, @include_numbers, @include_special_characters)
   erb :result
 end
